7d3c6194176641c74effdc9ed448aedf12e585cf
VCM admin account <vcm@vcm-8950.vm.duke.edu>
2019-04-09 23:25:15 -0400

    changerelation

--- ups_web/ups/models.py
+++ ups_web/ups/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.conf import settings
-import uuid
-from uuid import uuid4
 from django.dispatch import receiver
+import uuid
 
 class Profile(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this user")
+    #amazon pass the user id to ups when the purchase was made
+    userId = models.IntegerField(primary_key=True)
+    #id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     def __str__(self):
         """String for representing the Model object (in Admin site etc.)"""
