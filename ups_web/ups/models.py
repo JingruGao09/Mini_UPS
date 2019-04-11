a5b6502835d98d2df13e0a6d7db6dd3c4ad6e22c
VCM admin account <vcm@vcm-8950.vm.duke.edu>
2019-04-11 16:19:36 -0400

    not using sql

--- ups_web/ups/models.py
+++ ups_web/ups/models.py
@@ -6,126 +6,40 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import User
 
+class Truck(models.Model):
+    #package = models.ForeignKey(Package, models.DO_NOTHING, primary_key=True)
+    world = models.ForeignKey('World', models.DO_NOTHING)
+    truck_id = models.IntegerField(null = False)
+    TRUCK_STATUS_OP = (
+        ('1','idle'),
+        ('2','en route to a warehouse'),
+        ('3','invalid'),
+        ('4','delivering'),
+        ('5','waiting for pickup')
+    )
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=80)
-
+    truck_status = models.CharField(max_length=1,
+                                    choices = TRUCK_STATUS_OP,
+                                    blank = True,
+                                    default = '5',
+                                    null = True,
+                                    help_text='Truck Status')
     class Meta:
         managed = False
-        db_table = 'auth_group'
+        db_table = 'truck'
+        #unique_together = (('package', 'world'),)
 
-
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
+    def __str__(self):
+        return self.truck_id
 
 class Package(models.Model):
     package_id = models.IntegerField(primary_key=True)
     user = models.ForeignKey('UpsProfile', models.DO_NOTHING)
     world = models.ForeignKey('World', models.DO_NOTHING)
-    wh_id = models.IntegerField()
-    seqnum = models.IntegerField()
-    truck_id = models.IntegerField()
-    truck_status = models.CharField(max_length=20)
+    seqnum = models.IntegerField(null = False)
+    truck = models.ForeignKey('Truck',models.DO_NOTHING)
     x = models.IntegerField()
     y = models.IntegerField()
 
@@ -133,35 +47,33 @@ class Package(models.Model):
         managed = False
         db_table = 'package'
 
+    def __str__(self):
+        return package_id
+
 
 class Shipment(models.Model):
     ship_id = models.AutoField(primary_key=True)
     package = models.ForeignKey(Package, models.DO_NOTHING)
     world = models.ForeignKey('World', models.DO_NOTHING)
-    status = models.CharField(max_length=50)
+    #packing, packed, loading, loaded, delivering, delivered
+    #status = models.CharField(max_length=50)
 
     class Meta:
         managed = False
         db_table = 'shipment'
 
-
-class Truck(models.Model):
-    package = models.ForeignKey(Package, models.DO_NOTHING, primary_key=True)
-    world = models.ForeignKey('World', models.DO_NOTHING)
-    truck_id = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'truck'
-        unique_together = (('package', 'world'),)
-
+    def __str__(self):
+        return ship_id
 
 class UpsProfile(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING, unique=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, unique=True)
 
     class Meta:
         managed = False
         db_table = 'ups_profile'
+
+    def __str__(self):
+        return self.user.__str__()
 
 
 class World(models.Model):
@@ -170,3 +82,6 @@ class World(models.Model):
     class Meta:
         managed = False
         db_table = 'world'
+
+    def __str__(self):
+        return world_id
