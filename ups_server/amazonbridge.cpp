6b843f2ae8a2da3ac67d69e55def8ee3e1cf3cc8
jg404 <vcm@vcm-7989.vm.duke.edu>
2019-04-15 19:08:36 -0400

    ups_amazon_header edited

--- ups_server/amazonbridge.cpp
+++ ups_server/amazonbridge.cpp
@@ -3,6 +3,17 @@
 AmazonBridge::AmazonBridge(const char *hostname, const char *port)
     : ConAmazonClient(hostname, port) {}
 AmazonBridge::~AmazonBridge() {}
+
+/*
+ * Send World Id to Amazon
+ * Send msg to inform Amazon world to connect
+ * If success return 0, else, return -1
+ */
 int AmazonBridge::SendWorldId(const int &world_id){
-  
+  UPS_AMAZON::InitWorld msg;
+  msg.set_worldid(5);
+  msg.set_seqnum(1);
+  return SendMsg<UPS_AMAZON::InitWorld>(msg);
 }
+
+
--- ups_server/amazonbridge.h
+++ ups_server/amazonbridge.h
@@ -2,6 +2,11 @@
 #define __AMAZONBRIDGE_H__
 #include "client.h"
 #include "server.h"
+#include "ups_amazon.pb.h"
+#include "dbinterface.h"
+#include <iostream>
+#include <string>
+
 // client will do connection and garbage collection
 class AmazonBridge {
 private:
