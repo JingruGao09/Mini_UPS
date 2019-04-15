63ba942b7f2faca776209cd9e5451a06850e533b
jg404 <vcm@vcm-7989.vm.duke.edu>
2019-04-15 17:34:37 -0400

    amazonbridge

--- ups_server/amazonbridge.h
+++ ups_server/amazonbridge.h
@@ -1,12 +1,12 @@
 #ifndef __AMAZONBRIDGE_H__
 #define __AMAZONBRIDGE_H__
 #include "client.h"
-
+#include "server.h"
 // client will do connection and garbage collection
 class AmazonBridge {
 private:
-  Client Hermes;
-
+  Client ConAmazonClient;
+  Server UpsServer;
 public:
   AmazonBridge(const char *hostname, const char *port);
   ~AmazonBridge();
