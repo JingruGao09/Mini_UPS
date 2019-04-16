947bf6002f05bb0715852f92e58e9961511fc6bc
jg404 <vcm@vcm-7989.vm.duke.edu>
2019-04-16 16:48:44 -0400

    fixedemacs

--- ups_server/amazonbridge.cpp
+++ ups_server/amazonbridge.cpp
@@ -9,17 +9,13 @@ AmazonBridge::~AmazonBridge() {}
  * Send msg to inform Amazon world to connect
  * If success return 0, else, return -1
  */
-int AmazonBridge::SendWorldId(const int &world_id){
+int AmazonBridge::SendWorldId(const int &world_id) {
   UA::InitWorld msg;
   msg.set_worldid(world_id);
   msg.set_seqnum(1);
   return SendMsg<UA::InitWorld>(msg);
 }
 
-int AmazonBridge::SendTruckId(){
+int AmazonBridge::SendTruckId(const int &truck_id) {}
 
-}
-
-int AmazonBridge::SendPackageId(){
-
-}
+int AmazonBridge::SendPackageId() {}
