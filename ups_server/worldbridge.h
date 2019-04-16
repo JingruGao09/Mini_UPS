336091df4e55660eb3f84a0a28df45f63c99a203
Yuefan <361655243@qq.com>
2019-04-16 18:00:28 -0400

    pick and deliver package

--- ups_server/worldbridge.h
+++ ups_server/worldbridge.h
@@ -6,10 +6,24 @@
 #include <iostream>
 #include <string>
 #define TRUCK_NUM 200
+struct _truck_t {
+  int truck_id;
+  int x;
+  int y;
+};
+struct _package_t {
+  int package_id;
+  int x;
+  int y;
+};
+typedef struct _package_t package_t;
+typedef struct _truck_t truck_t;
 class WorldBridge {
 private:
+  int world_id;
   std::string errmsg;
   Client Hermes;
+  DBInterface Zeus;
   /* SendMsg
    * easy way to send google protocol message, return 0 if success
    * else return -1, check errmsg to find some idea
@@ -24,14 +38,19 @@ private:
     }
     return 0;
   }
+  int CreateTrucks(int truckNum, UPS::UConnect &msg);
+  int SetPackageInfo(truck_t &truck, std::vector<package_t> &packages,
+                     UPS::UGoDeliver *goDeliver);
 
 public:
   WorldBridge(const char *hostname, const char *port);
   ~WorldBridge();
   int RequireANewWorld();
-  int ConnectToAWorld(int64_t world_id, bool initTruck);
-  int CreateTrucks(int truckNum, UPS::UConnect &msg, const int &world_id);
-  int GoPickUp();
-  int GoDeliver();
+  int ConnectToAWorld(const int64_t &wid, bool initTruck);
+
+  int GoPickUp(const int &wh_id, std::vector<truck_t> &trucks,
+               const int64_t &seqnum);
+  int GoDeliver(truck_t &truck, std::vector<package_t> &packages,
+                const int64_t &seqnum);
 };
 #endif
