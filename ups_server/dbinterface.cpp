279bb4bc1135c27366047211914040bdd2a3c797
Yuefan <361655243@qq.com>
2019-04-18 10:39:18 -0400

    update db name

--- ups_server/dbinterface.cpp
+++ ups_server/dbinterface.cpp
@@ -6,7 +6,7 @@ DBInterface::DBInterface() {
   // Establish a connection to the database
   // Parameters: database name, user name, user password
   C = std::unique_ptr<pqxx::connection>(
-      new pqxx::connection("dbname=EXCHANGE user=postgres password=passw0rd"));
+      new pqxx::connection("dbname=UPS user=postgres password=passw0rd"));
   if (C->is_open()) {
     //  cout << "Opened database successfully: " << C->dbname() << endl;
   } else {
