7961159b98dd066d80990c87637b166f88a794a5
jg404 <vcm@vcm-7989.vm.duke.edu>
2019-04-22 15:17:45 -0400

    docker not working

--- ups_web/ups_web/settings.py
+++ ups_web/ups_web/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'UPS',
         'USER': 'postgres',
         'PASSWORD':'passw0rd',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
