c0178bc7163d3ae0e8177d45901f4d1a52ab7a20
Yuefan <361655243@qq.com>
2019-04-23 23:54:11 -0400

    can run

--- ups_server/Mini_ups.cpp
+++ ups_server/Mini_ups.cpp
@@ -6,10 +6,11 @@ void A_listen_thread(UPServer &upserver);
 int main() {
   while (1) {
     try {
-      UPServer upserver("vcm-9320.vm.duke.edu", "12345", "10.197.40.0", "7893");
+      // UPServer upserver("vcm-9320.vm.duke.edu", "12345", "10.197.40.0",
+      // "7893");
 
-      // UPServer upserver("vcm-8129.vm.duke.edu", "12345", "10.197.193.1",
-      //                  "12321");
+      UPServer upserver("vcm-8129.vm.duke.edu", "12345", "10.197.193.1",
+                        "20000");
       // UPServer upserver(argv[1], argv[2], argv[3], argv[4]);
       // UPServer upserver("vcm-8129.vm.duke.edu", "12345", "10.197.193.1",
       // "80");
