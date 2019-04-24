f254208046d0852dc09c0059758053480f708e05
Yuefan <361655243@qq.com>
2019-04-24 13:53:41 -0400

    package view

--- ups_web/ups/urls.py
+++ ups_web/ups/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='ups/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(), name='logout'),
     path('myPackages/',views.MyPackagesView,name='myPackages'),
-    path('package/<int:pk>',views.PackageDetailView.as_view(),name='package-detail'),
-    #path('package/<int:pk>', views.PackageDetail, name='package-detail'),
+    #path('package/<int:pk>',views.PackageDetailView.as_view(),name='package-detail'),
+    path('package/<int:pk>', views.PackageDetail, name='package-detail'),
     #path('matchmypackage/',views.MatchMyPackageView,name = 'MatchMyPackageView')
     path('editmypackagedest/<int:package_id>',views.EditMyPackageDestView,name = 'EditMyPackageDestView'),
 ]
